01e8abf0d6bb7991548dd48a166b18d426f76792
Rick Yang <rick.yang@cgi.com>
2025-01-15 16:08:51 -0500

    updating to app-v-3

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Rick Jenkins Pipeline
+          Learn Jenkins
         </a>
       </header>
       <p>
